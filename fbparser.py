2cb0a26064dd1820fb8e487ec8ec866d2f537253
Daine McNiven <daine.mcniven@hotmail.com>
2018-09-28 18:01:46 -0600

    Made more generic - read names from conversation and used these instead of hardcoding our names.

--- fbparser.py
+++ fbparser.py
@@ -33,7 +33,7 @@ def to_percent(top, bottom):
         colorB: The color of the yValsB data.
         barwidth: The width of the bars.
 """
-def make_barchart(numX, yValsA, yValsB, title='', ylabel='Number of Messages', xticks=None, xticksorient='vertical', yValsAName='Tessa', yValsBName='Daine', colorA='purple', colorB='aqua', barwidth=0.4):
+def make_barchart(numX, yValsA, yValsB, title='', ylabel='Number of Messages', xticks=None, xticksorient='vertical', yValsAName='P2', yValsBName='P1', colorA='purple', colorB='aqua', barwidth=0.4):
     make_barchart.num_charts += 1
     plt.figure(make_barchart.num_charts)
     plt.subplot(111)
@@ -54,7 +54,7 @@ if len(sys.argv) > 3 or len(sys.argv) == 1 or (len(sys.argv) == 3 and sys.argv[2
     exit()
 
 print 'Starting program...\n'
-print 'Opening file ', './TestFiles/DaineMcNiven/message.json', '...'
+print 'Opening file ', sys.argv[1], '...'
 with open(sys.argv[1]) as f:
     print 'Loading data from file ...'
     data = json.load(f)
@@ -81,80 +81,89 @@ total_days = (total_time / 60 / 60 / 24)
 print 'Parsing data...'
 
 # Parse messages by sender
-messages_from_daine = [x for x in list_of_messages if x['sender_name'] == u'Daine McNiven']
-messages_from_tessa = [x for x in list_of_messages if x['sender_name'] == u'Tessa Heiydt']
-#messages_from_other = [x for x in list_of_messages if x['sender_name'] != u'Daine McNiven' and x['sender_name'] != u'Tessa Heiydt'] #should be empty
+people_in_conversation = []
+for x in list_of_messages:
+    if x['sender_name'] not in people_in_conversation:
+        people_in_conversation.append(x['sender_name'])
+if len(people_in_conversation) < 2:
+    print 'Must have at least two people in the conversation.'
+    exit()
+if len(people_in_conversation) > 2:
+    print 'This program is meant for only 2 person conversations, using ', people_in_conversation[0], ' and ', people_in_conversation[1], '.'
 
-num_messages_tessa = len(messages_from_tessa)
-num_messages_daine = len(messages_from_daine)
+messages_from_p1 = [x for x in list_of_messages if x['sender_name'] == people_in_conversation[0]]
+messages_from_p2 = [x for x in list_of_messages if x['sender_name'] == people_in_conversation[1]]
+
+num_messages_p1 = len(messages_from_p1)
+num_messages_p2 = len(messages_from_p2)
 
 # Parse content of messages by each sender
-content_from_daine = [d['content'].lower() for d in messages_from_daine if 'content' in d]
-content_from_tessa = [d['content'].lower() for d in messages_from_tessa if 'content' in d]
+content_from_p1 = [d['content'].lower() for d in messages_from_p1 if 'content' in d]
+content_from_p2 = [d['content'].lower() for d in messages_from_p2 if 'content' in d]
 
-most_common_messages_daine = Counter(content_from_daine).most_common(10)
-most_common_messages_tessa = Counter(content_from_tessa).most_common(10)
+most_common_messages_p1 = Counter(content_from_p1).most_common(10)
+most_common_messages_p2 = Counter(content_from_p2).most_common(10)
 
 # Parse words from each message
-words_from_daine = [word for l in content_from_daine for word in l.split()]
-words_from_daine_no_stopwords = [x for x in words_from_daine if x not in g_stopwords]
-words_from_tessa = [word for l in content_from_tessa for word in l.split()]
-words_from_tessa_no_stopwords = [x for x in words_from_tessa if x not in g_stopwords]
+words_from_p1 = [word for l in content_from_p1 for word in l.split()]
+words_from_p1_no_stopwords = [x for x in words_from_p1 if x not in g_stopwords]
+words_from_p2 = [word for l in content_from_p2 for word in l.split()]
+words_from_p2_no_stopwords = [x for x in words_from_p2 if x not in g_stopwords]
 
-num_words_daine = len(words_from_daine)
-num_words_tessa = len(words_from_tessa)
+num_words_p1 = len(words_from_p1)
+num_words_p2 = len(words_from_p2)
 
-most_common_words_daine = Counter(words_from_daine_no_stopwords).most_common(10)
-most_common_words_tessa = Counter(words_from_tessa_no_stopwords).most_common(10)
+most_common_words_p1 = Counter(words_from_p1_no_stopwords).most_common(10)
+most_common_words_p2 = Counter(words_from_p2_no_stopwords).most_common(10)
 
-num_unique_words_daine = len(set(words_from_daine))
-num_unique_words_tessa = len(set(words_from_tessa))
+num_unique_words_p1 = len(set(words_from_p1))
+num_unique_words_p2 = len(set(words_from_p2))
 
 if len(sys.argv) != 3:
     # Find messages by hours
-    messages_by_hour_daine = [0] * 24
-    messages_by_hour_tessa = [0] * 24
+    messages_by_hour_p1 = [0] * 24
+    messages_by_hour_p2 = [0] * 24
     for i in range(0, 24):
-        messages_by_hour_daine[i] = len([x for x in messages_from_daine if datetime.datetime.fromtimestamp(int(x['timestamp'])).hour == i])
-        messages_by_hour_tessa[i] = len([x for x in messages_from_tessa if datetime.datetime.fromtimestamp(int(x['timestamp'])).hour == i])
+        messages_by_hour_p1[i] = len([x for x in messages_from_p1 if datetime.datetime.fromtimestamp(int(x['timestamp'])).hour == i])
+        messages_by_hour_p2[i] = len([x for x in messages_from_p2 if datetime.datetime.fromtimestamp(int(x['timestamp'])).hour == i])
 
     xticks = tuple([datetime.time(i).strftime('%I %p') for i in range(24)])
-    make_barchart(24, messages_by_hour_tessa, messages_by_hour_daine, 'Number of Messages by Hour', xticks=xticks)
+    make_barchart(24, messages_by_hour_p2, messages_by_hour_p1, 'Number of Messages by Hour', xticks=xticks, yValsAName = people_in_conversation[1], yValsBName = people_in_conversation[0])
 
     # Find messages by Day of Week
-    messages_by_dayofweek_daine = [0] * 7
-    messages_by_dayofweek_tessa = [0] * 7
+    messages_by_dayofweek_p1 = [0] * 7
+    messages_by_dayofweek_p2 = [0] * 7
     for i in range(0, 7):
-        messages_by_dayofweek_daine[i] = len([x for x in messages_from_daine if datetime.datetime.fromtimestamp(int(x['timestamp'])).weekday() == i])
-        messages_by_dayofweek_tessa[i] = len([x for x in messages_from_tessa if datetime.datetime.fromtimestamp(int(x['timestamp'])).weekday() == i])
+        messages_by_dayofweek_p1[i] = len([x for x in messages_from_p1 if datetime.datetime.fromtimestamp(int(x['timestamp'])).weekday() == i])
+        messages_by_dayofweek_p2[i] = len([x for x in messages_from_p2 if datetime.datetime.fromtimestamp(int(x['timestamp'])).weekday() == i])
 
-    make_barchart(7, messages_by_dayofweek_tessa, messages_by_dayofweek_daine, 'Number of Messages by Day of Week', xticks=calendar.day_abbr)
+    make_barchart(7, messages_by_dayofweek_p2, messages_by_dayofweek_p1, 'Number of Messages by Day of Week', xticks=calendar.day_abbr, yValsAName = people_in_conversation[1], yValsBName = people_in_conversation[0])
 
     # Find messages by Month
-    messages_by_month_daine = [0] * 12
-    messages_by_month_tessa = [0] * 12
+    messages_by_month_p1 = [0] * 12
+    messages_by_month_p2 = [0] * 12
     for i in range(1, 13):
-        messages_by_month_daine[i-1] = len([x for x in messages_from_daine if datetime.datetime.fromtimestamp(int(x['timestamp'])).month == i])
-        messages_by_month_tessa[i-1] = len([x for x in messages_from_tessa if datetime.datetime.fromtimestamp(int(x['timestamp'])).month == i])
+        messages_by_month_p1[i-1] = len([x for x in messages_from_p1 if datetime.datetime.fromtimestamp(int(x['timestamp'])).month == i])
+        messages_by_month_p2[i-1] = len([x for x in messages_from_p2 if datetime.datetime.fromtimestamp(int(x['timestamp'])).month == i])
 
-    make_barchart(12, messages_by_month_tessa, messages_by_month_daine, 'Number of Messages by Month', xticks=calendar.month_abbr[1:13])
+    make_barchart(12, messages_by_month_p2, messages_by_month_p1, 'Number of Messages by Month', xticks=calendar.month_abbr[1:13], yValsAName = people_in_conversation[1], yValsBName = people_in_conversation[0])
 
     # Find messages by Year
-    messages_by_year_daine = [0] * numYears
-    messages_by_year_tessa = [0] * numYears
+    messages_by_year_p1 = [0] * numYears
+    messages_by_year_p2 = [0] * numYears
     for i in range(yearBegin, yearEnd+1):
-        messages_by_year_daine[i-yearBegin] = len([x for x in messages_from_daine if datetime.datetime.fromtimestamp(int(x['timestamp'])).year == i])
-        messages_by_year_tessa[i-yearBegin] = len([x for x in messages_from_tessa if datetime.datetime.fromtimestamp(int(x['timestamp'])).year == i])
+        messages_by_year_p1[i-yearBegin] = len([x for x in messages_from_p1 if datetime.datetime.fromtimestamp(int(x['timestamp'])).year == i])
+        messages_by_year_p2[i-yearBegin] = len([x for x in messages_from_p2 if datetime.datetime.fromtimestamp(int(x['timestamp'])).year == i])
 
-    make_barchart(numYears, messages_by_year_tessa, messages_by_year_daine, 'Number of Messages by Year', xticks=tuple(range(yearBegin, yearEnd+1)))
+    make_barchart(numYears, messages_by_year_p2, messages_by_year_p1, 'Number of Messages by Year', xticks=tuple(range(yearBegin, yearEnd+1)), yValsAName = people_in_conversation[1], yValsBName = people_in_conversation[0])
 
     # Find messages by Year & Month
-    messages_by_yearmonth_daine = [0] * (12 * numYears)
-    messages_by_yearmonth_tessa = [0] * (12 * numYears)
+    messages_by_yearmonth_p1 = [0] * (12 * numYears)
+    messages_by_yearmonth_p2 = [0] * (12 * numYears)
     for i in range(yearBegin, yearEnd+1):
         for j in range(1, 13):
-            messages_by_yearmonth_daine[(i-yearBegin)*12+(j-1)] = len([x for x in messages_from_daine if (datetime.datetime.fromtimestamp(int(x['timestamp'])).year == i and datetime.datetime.fromtimestamp(int(x['timestamp'])).month == j)])
-            messages_by_yearmonth_tessa[(i-yearBegin)*12+(j-1)] = len([x for x in messages_from_tessa if (datetime.datetime.fromtimestamp(int(x['timestamp'])).year == i and datetime.datetime.fromtimestamp(int(x['timestamp'])).month == j)])
+            messages_by_yearmonth_p1[(i-yearBegin)*12+(j-1)] = len([x for x in messages_from_p1 if (datetime.datetime.fromtimestamp(int(x['timestamp'])).year == i and datetime.datetime.fromtimestamp(int(x['timestamp'])).month == j)])
+            messages_by_yearmonth_p2[(i-yearBegin)*12+(j-1)] = len([x for x in messages_from_p2 if (datetime.datetime.fromtimestamp(int(x['timestamp'])).year == i and datetime.datetime.fromtimestamp(int(x['timestamp'])).month == j)])
 
     xticks = []
 
@@ -162,9 +171,8 @@ if len(sys.argv) != 3:
         xticks.append(str(year))
         for i in range(0, 11):
             xticks.append('')
-    make_barchart(12*(numYears), messages_by_yearmonth_tessa, messages_by_yearmonth_daine, 'Number of Messages by Year and Month', xticks=xticks)
+    make_barchart(12*(numYears), messages_by_yearmonth_p2, messages_by_yearmonth_p1, 'Number of Messages by Year and Month', xticks=xticks, yValsAName = people_in_conversation[1], yValsBName = people_in_conversation[0])
 
-#plt.show()
     plt.draw()
 
 # Print data
@@ -172,44 +180,44 @@ print '\nSummary\n======='
 print 'Total messages: ', num_messages, ' from ', first_message_time, ' to ', last_message_time
 print 'That\'s ', total_days, ' days and ', num_messages/total_days, ' messages per day!\n'
 
-# Tessa
-print 'Tessa:\n-----\n', num_messages_tessa, ' messages (', to_percent(num_messages_tessa, num_messages), ')'
+# P2
+print people_in_conversation[1], ':\n-----\n', num_messages_p2, ' messages (', to_percent(num_messages_p2, num_messages), ')'
 
 # Messages
 print 'Most Common Messages'
-max_val = min(10, len(most_common_messages_tessa)-1)
+max_val = min(10, len(most_common_messages_p2)-1)
 for i in range(0, max_val):
-    print '\t', i+1, ': "', most_common_messages_tessa[i][0], '" said ', most_common_messages_tessa[i][1], ' times (', to_percent(int(most_common_messages_tessa[i][1]), num_messages_tessa), '),'
-print '\t', max_val+1, ': ', most_common_messages_tessa[max_val][0], '" said ',most_common_messages_tessa[max_val][1], ' times (', to_percent(int(most_common_messages_tessa[max_val][1]), num_messages_tessa), ').'
+    print '\t', i+1, ': "', most_common_messages_p2[i][0], '" said ', most_common_messages_p2[i][1], ' times (', to_percent(int(most_common_messages_p2[i][1]), num_messages_p2), '),'
+print '\t', max_val+1, ': ', most_common_messages_p2[max_val][0], '" said ',most_common_messages_p2[max_val][1], ' times (', to_percent(int(most_common_messages_p2[max_val][1]), num_messages_p2), ').'
 
 #Words
 print 'Most Common Words'
-max_val = min(10, len(most_common_words_tessa)-1)
+max_val = min(10, len(most_common_words_p2)-1)
 for i in range(0, max_val):
-    print '\t', i+1, ': "', most_common_words_tessa[i][0], '" said ', most_common_words_tessa[i][1], ' times (', to_percent(int(most_common_words_tessa[i][1]), num_words_tessa), '),'
-print '\t', max_val+1, ': "', most_common_words_tessa[max_val][0], '" said ',most_common_words_tessa[max_val][1], ' times (', to_percent(int(most_common_words_tessa[max_val][1]), num_words_tessa), ').'
+    print '\t', i+1, ': "', most_common_words_p2[i][0], '" said ', most_common_words_p2[i][1], ' times (', to_percent(int(most_common_words_p2[i][1]), num_words_p2), '),'
+print '\t', max_val+1, ': "', most_common_words_p2[max_val][0], '" said ',most_common_words_p2[max_val][1], ' times (', to_percent(int(most_common_words_p2[max_val][1]), num_words_p2), ').'
 
-print 'Unique Words Said: ', num_unique_words_tessa, ' giving a diversity index of: ', to_percent(num_unique_words_tessa, num_words_tessa)
+print 'Unique Words Said: ', num_unique_words_p2, ' giving a diversity index of: ', to_percent(num_unique_words_p2, num_words_p2)
 
 
-# Daine
-print 'Daine:\n-----\n', num_messages_daine, ' messages (', to_percent(num_messages_daine, num_messages), ')'
+# P1
+print people_in_conversation[0], ':\n-----\n', num_messages_p1, ' messages (', to_percent(num_messages_p1, num_messages), ')'
 
 # Messages
 print 'Most Common Messages'
-max_val = min(10, len(most_common_messages_daine)-1)
+max_val = min(10, len(most_common_messages_p1)-1)
 for i in range(0, max_val):
-    print '\t', i+1, ': "', most_common_messages_daine[i][0], '" said ', most_common_messages_daine[i][1], ' times (', to_percent(int(most_common_messages_daine[i][1]), num_messages_daine), '),'
-print '\t', max_val+1, ': "', most_common_messages_daine[max_val][0], '" said ',most_common_messages_daine[max_val][1], ' times (', to_percent(int(most_common_messages_daine[max_val][1]), num_messages_daine), ').'
+    print '\t', i+1, ': "', most_common_messages_p1[i][0], '" said ', most_common_messages_p1[i][1], ' times (', to_percent(int(most_common_messages_p1[i][1]), num_messages_p1), '),'
+print '\t', max_val+1, ': "', most_common_messages_p1[max_val][0], '" said ',most_common_messages_p1[max_val][1], ' times (', to_percent(int(most_common_messages_p1[max_val][1]), num_messages_p1), ').'
 
 # Words
 print 'Most Common Words'
-max_val = min(10, len(most_common_words_daine)-1)
+max_val = min(10, len(most_common_words_p1)-1)
 for i in range(0, max_val):
-    print '\t', i+1, ': "', most_common_words_daine[i][0], '" said ', most_common_words_daine[i][1], ' times (', to_percent(int(most_common_words_daine[i][1]), num_words_daine), '),'
-print '\t', max_val+1, ': "', most_common_words_daine[max_val][0], '" said ',most_common_words_daine[max_val][1], ' times (', to_percent(int(most_common_words_daine[max_val][1]), num_words_daine), ').'
+    print '\t', i+1, ': "', most_common_words_p1[i][0], '" said ', most_common_words_p1[i][1], ' times (', to_percent(int(most_common_words_p1[i][1]), num_words_p1), '),'
+print '\t', max_val+1, ': "', most_common_words_p1[max_val][0], '" said ',most_common_words_p1[max_val][1], ' times (', to_percent(int(most_common_words_p1[max_val][1]), num_words_p1), ').'
 
-print 'Unique Words Said: ', num_unique_words_daine, ' giving a diversity index of: ', to_percent(num_unique_words_daine, num_words_daine)
+print 'Unique Words Said: ', num_unique_words_p1, ' giving a diversity index of: ', to_percent(num_unique_words_p1, num_words_p1)
 
 if len(sys.argv) != 3:
     plt.show()
